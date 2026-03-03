583c2ad2ea82b093443b31aae4cc702aa54005a6
wasted <67445572+just-wasted@users.noreply.github.com>
2026-03-03 15:47:11 +0100

    spielwelt stuff implemented, lol

--- project/oop_basics/oop_basics/Spielwelt.cs
+++ project/oop_basics/oop_basics/Spielwelt.cs
@@ -2,12 +2,56 @@
 
 public class Spielwelt
 {
-	public Spielwelt()
-	{
+    public int Size_x
+    {
+        get
+        {
+            return _size_x;
+        }
     }
+
+    public int Size_y
+    {
+        get
+        {
+            return _size_y;
+        }
+    }
+    private const int _size_x = 25;
+    private const int _size_y = 25;
+    public Spielfigur[,] board = Spielfigur[_size_x, _size_y];
+
+    private static Spielwelt? _instance;
+
+    private Spielwelt()
+    {
+    }
+
+    public static Spielwelt GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Spielwelt();
+        }
+        return _instance;
+    }
+
+    public void SpawnPlayer(Spielfiegur Figur, int x_pos, int y_pos)
+    {
+        if (!besetzt(x_pos, y_pos))
+        {
+            board[x_pos, y_pos] = Figur;
+        }
+
+    }
+
     public bool besetzt(int x_pos, int y_pos)
     {
-        // bitte gemß Plan implementieren
-        // Console.WriteLine("Welt ausgegeben");
+        if (board[x_pos, y_pos] != null)
+        {
+            return true;
+        }
+        return false;
+
     }
 }
