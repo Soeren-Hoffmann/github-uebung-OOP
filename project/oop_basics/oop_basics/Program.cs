e82ff73050b94f4966cc2dcc5de775ae73c267f7
Sören (Dozent) <Soeren.Hoffmann@Qualidy.de>
2026-03-03 14:29:05 +0100

    feat: add code project structure

---
+++ project/oop_basics/oop_basics/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Spielwelt welt = new Spielwelt();
+        welt.besetzt(0, 0);
+    }
+}
---
+++ project/oop_basics/oop_basics/Spielwelt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Spielwelt
+{
+	public Spielwelt()
+	{
+    }
+    public bool besetzt(int x_pos, int y_pos)
+    {
+        // bitte gemß Plan implementieren
+        // Console.WriteLine("Welt ausgegeben");
+    }
+}
