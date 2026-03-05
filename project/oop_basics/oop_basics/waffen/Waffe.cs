b10057d0ecaf85c1766cbf5a9142308e7842ea6f
Soeren-Hoffmann <Soeren.Hoffmann@Qualidy.de>
2026-03-05 14:36:03 +0100

    fix: secure properties of weapons

--- project/oop_basics/oop_basics/waffen/Waffe.cs
+++ project/oop_basics/oop_basics/waffen/Waffe.cs
@@ -2,9 +2,9 @@ using System.Runtime.CompilerServices;
 
 public class Waffe
 {
-    public int Schaden { set; get; }
-    public int Reichweite { set; get; }
-    public string Schadensart { set; get; } = string.Empty;
+    public int Schaden { private set; get; }
+    public int Reichweite { private set; get; }
+    public string Schadensart { private set; get; } = string.Empty;
 
     public Waffe(int schaden, int reichweite, string schadensart)
     {
